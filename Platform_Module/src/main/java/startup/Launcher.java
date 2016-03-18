1c6a8022d72175baba968f9dc9ba4c943b5c1dc1
Pedro Colon <pedrocolon93@gmail.com>
2016-03-18 10:31:30 -0400

    Service wrapper

---
+++ Platform_Module/src/main/java/startup/Launcher.java
@@ -0,0 +1,34 @@
+package startup;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launcher
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    /**
+     * Method that starts the platform as a service
+     */
+    public static void start(){
+
+    }
+
+    /**
+     * Method that stops the platform service
+     */
+    public static void stop(){
+
+    }
+
+    /**
+     * Method that installs the service into the system for later starting/stopping
+     */
+    public static void install(){
+
+    }
+}
---
+++ Platform_Module/src/main/java/startup/yajsw-beta-12.05/src/yajsw/src/main/java/com/sun/jna/PlatformEx.java
@@ -0,0 +1,46 @@
+package com.sun.jna;
+
+public class PlatformEx
+{
+	private static boolean winVista = false;
+	private static boolean macYosemite = false;
+
+	static
+	{
+		String osName = System.getProperty("os.name").toLowerCase();
+		if (osName.startsWith("windows"))
+		{
+			winVista = osName.contains("vista") || osName.contains(" 7")
+					|| osName.contains("2008") || osName.contains("2012")
+					|| osName.contains(" 8");
+		}
+	}
+
+	static
+	{
+		if (Platform.isMac())
+			try
+			{
+				String[] versionStr = System.getProperty("os.version").split(
+						"\\.");
+				int maj = Integer.parseInt(versionStr[0]);
+				int min = Integer.parseInt(versionStr[1]);
+				macYosemite = (maj == 10 && min >= 10) || maj > 10;
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+	}
+
+	public static boolean isWinVista()
+	{
+		return winVista;
+	}
+
+	public static boolean isMacYosemite()
+	{
+		return macYosemite;
+	}
+
+}

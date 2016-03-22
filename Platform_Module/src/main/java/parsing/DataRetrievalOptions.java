1e90c8a74bc004329a4b9e006cd55eb52c2fe596
marieann <marie.nazario1@upr.edu>
2016-03-21 22:57:10 -0700

    return added

--- Platform_Module/src/main/java/parsing/DataRetrievalOptions.java
+++ Platform_Module/src/main/java/parsing/DataRetrievalOptions.java
@@ -7,18 +7,18 @@ public class DataRetrievalOptions extends Options {
     public DataRetrievalOptions(){}
 
     public String getLogstashPort(){
-    
+        return "";
     }
     
     public String getLogstashAddress(){
-    
+        return "";
     }
     
     public String getElasticSearchPort(){
-    
+        return "";
     }
    
     public String getElasticSearchAddress(){
-    
+        return "";
     }
 }
--- Platform_Module/src/main/java/parsing/UtilitiesOptions.java
+++ Platform_Module/src/main/java/parsing/UtilitiesOptions.java
@@ -4,18 +4,13 @@ package parsing;
  * Created by pedro on 3/18/16.
  */
 public class UtilitiesOptions extends Options{
-    private Options utilities = new Options();
-    
     public UtilitiesOptions(){}
     
     public String getPath(){
-        return null;
+        return "";
     }
     
-    public boolean setPath(){
-        return true;
-    }
     public String getFormattingFile() {
-        return null;
+        return "";
     }
 }

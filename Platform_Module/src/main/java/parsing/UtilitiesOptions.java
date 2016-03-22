86a61d3db11953196b8d3db48a633ec738b0f1f3
marieann <marie.nazario1@upr.edu>
2016-03-21 20:57:30 -0700

    .

--- Platform_Module/src/main/java/parsing/UtilitiesOptions.java
+++ Platform_Module/src/main/java/parsing/UtilitiesOptions.java
@@ -4,6 +4,8 @@ package parsing;
  * Created by pedro on 3/18/16.
  */
 public class UtilitiesOptions extends Options{
+    private Options utilities = new Options();
+    
     public UtilitiesOptions(){}
     
     public String getPath(){

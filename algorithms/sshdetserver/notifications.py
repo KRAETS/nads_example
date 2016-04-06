6c77f9571d5dcbd8ca8d9d9fb163290692cbb22c
Pedro Colon <pedrocolon93@gmail.com>
2016-04-05 23:07:49 -0400

    Integrating notification module with this algorithm

--- algorithms/sshdetserver/notifications.py
+++ algorithms/sshdetserver/notifications.py
@@ -1,12 +1,27 @@
+import urllib2
+NOTIFICATION_SYSTEM_ADDRESS = "localhost:2000"
 def notify_both(message):
     if message == "Singleton":
         f = open("singletonresults.txt","w+")
         f.write("yes")
         f.close()
+        try:
+            request = NOTIFICATION_SYSTEM_ADDRESS + "/" + str(1) + "**" + "Singleton Attack Detected:"
+            req = urllib2.Request(request)
+            response = urllib2.urlopen(req)
+        except Exception as e:
+            print "Could not send message:",e
+
     elif message == "Distributed":
         f = open("distributedresults.txt", "w+")
         f.write("yes")
         f.close()
+        try:
+            request = NOTIFICATION_SYSTEM_ADDRESS + "/" + str(1) + "**" + "Distributed Attack Detected:"
+            req = urllib2.Request(request)
+            response = urllib2.urlopen(req)
+        except Exception as e:
+            print "Could not send message:", e
     else:
         print "notifying!!", message
 

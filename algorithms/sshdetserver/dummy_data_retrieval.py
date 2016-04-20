08df4ce2e48963634af75fe4b83ccdc2c7bd443b
Pedro <antoine.cotto@upr.edu>
2016-04-19 21:24:51 -0300

    SMall fix to address

--- algorithms/sshdetserver/dummy_data_retrieval.py
+++ algorithms/sshdetserver/dummy_data_retrieval.py
@@ -18,7 +18,7 @@ def search(address, query):
     print "Requesting", completequery
 
     try:
-        response = requests.post(DATA_RET_SERVER_ADDRESS,query)
+        response = requests.post(DATA_RET_SERVER_ADDRESS+"/getdata",query)
 
         printthings = response.text
 

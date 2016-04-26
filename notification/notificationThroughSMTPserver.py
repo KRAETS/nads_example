1028d178352009c3f27e048ceeb668219152efaa
marieann <marie.nazario1@upr.edu>
2016-04-25 20:39:40 -0400

    server host included

--- notification/notificationThroughSMTPserver.py
+++ notification/notificationThroughSMTPserver.py
@@ -7,16 +7,14 @@ from flask import Flask, request
 
 # --------------------------------------------------------- Variables
 print 'Notification Script started'
-args = sys.argv  # argv(1) json string
+args = sys.argv
 size = 1024
 cellphoneComp = {'att': 'txt.att.net', 'att-cingular': 'mmode.com', 'sprint': 'messaging.sprintpcs.com',
-                 'claro': 'vtexto.com', 'tmobile': 'tmomail.net', 'openMobile': 'email.openmobilepr.com',
-                 'verizon': 'vtext.com'}
-emailComp = {'gmail': 'smtp.gmail.com', 'yahoo': 'smtp.mail.yahoo.com', 'hotmail': 'smtp.live.com'}
-#emaildatacheck = True
+                 'claro': 'vtexto.com', 'openMobile': 'email.openmobilepr.com', 'verizon': 'vtext.com'}
+emailComp = {'gmail': 'smtp.gmail.com', 'yahoo': 'smtp.mail.yahoo.com', 'hotmail': 'smtp.live.com',
+             'ece.uprm': 'smtps.ece.uprm.edu'}
 serverdatacheck = True
 email = ''
-#password = ''
 serverhost = ''
 smtp = None
 algs = dict()
@@ -24,10 +22,10 @@ validalgs = True
 
 # ------------------------------------------------------------ Set-up
 def setup():
-    """Extracts smtp login information and users to notify information."""
+    """Extracts smtp login and users to notify information."""
     try:
         print "Starting setup"
-        global email, serverhost, serverdatacheck, validalgs, algs #emaildatacheck, password,
+        global email, serverhost, serverdatacheck, validalgs, algs
 
         if len(args) > 1:  # ------- text and email set-up
             data = dict()
@@ -38,9 +36,8 @@ def setup():
             if len(args) > 2:  # valid args list information
                 print "\tLoading algorithms list", args[2]
                 argslist = json.loads(args[2])
-
                 for a in argslist["List"]:
-#                    print "Loading", a
+                    #print "Loading", a
                     algs[a] = []
             else:
                 validalgs = False
@@ -51,16 +48,13 @@ def setup():
                 email = args[3]
                 if len(args) > 4:
                     # print 'Loading email password'
-                    # password = args[4]
                     print "Loading SMTP server"
                     serverhost = args[4]
                 else:
-                    # print "Could not get password"
-                    # emaildatacheck = False
+                    # print "Could not get server host"
                     print "Could not get server data"
                     serverdatacheck = False
             else:
-                # emaildatacheck = False
                 serverdatacheck = False
 
             #print "Interpreting users  information"
@@ -89,7 +83,7 @@ def setup():
                         if nflag:
                             #print "Preparing to check algs"
                             listtocheck = []
-                            #listtocheck = json.loads(data.get(key)['notifiablealgorithms'])
+                            listtocheck = json.loads(data.get(key)['notifiablealgorithms'])
                             listtocheck = data.get(key)['notifiablealgorithms']
                             #print listtocheck
                             for a in listtocheck:
@@ -103,9 +97,9 @@ def setup():
                             'phonenumber'] + '\" phone number is incorrect and was not added to the notification list'
 
                 if 'email' in data.get(key):  # email set-up
-                    if re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", data.get(key)['email']):
+                    if re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$(\.[a-zA-Z]+)?", data.get(key)['email']):
                         listtocheck = []
-                        #listtocheck = json.loads(data.get(key)['notifiablealgorithms'])
+                        listtocheck = json.loads(data.get(key)['notifiablealgorithms'])
                         listtocheck = data.get(key)['notifiablealgorithms']
                         for a in listtocheck:
                             if a in algs.keys():
@@ -124,53 +118,28 @@ def setup():
 
 # --------------------------------------------- termination signal
 def signal_term_handler(a, b):
+    """Handling of termination signals"""
+    global smtp
     try:
         shutdown_server()
         print "Notification Module Successfully Killed"
 
     except Exception as e:
         print "Could not shut down", e
+    smtp.quit()
     sys.exit(0)
 
 # ---------------------------------------------------- smtp set-up
 def smtp_setup():
     """Sets up smtp configuration and login"""
     print "Smtp setup starting"
-    global smtp, serverdatacheck, email, serverhost # emaildatacheck,
-    # if emaildatacheck:
-    #     print "Evaluating smtp email"
-    #     start = email.lower().find('@') + 1
-    #     end = len(email.lower())
-    #     emailprovider = email[start:end].lower()
-    #     option = []
-    #     print "Finding provider"
-    #     for key in emailComp:
-    #         if emailprovider in emailComp[key]:
-    #             option = [emailComp[key], 465]
-    #             break
-    #     try:
-    #         print "Establishing ssl"
-    #         smtp = smtplib.SMTP_SSL(option[0], option[1])
-    #         print "Success"
-    #     except Exception as e:
-    #         print "ERROR: Could not establish ssl connection", str(e)
-    #         sys.exit(1)
-    #
-    #     print "Ssl established."
-    #
-    #     print "Trying smtp Login"
-    #     if str(smtp.ehlo()[0]) == '250':
-    #         try:
-    #             res = smtp.login(email, password)
-    #             print "Login result: ", res
-    #
-    #         except Exception as e:
-    #             print 'Problem while attempting login ', str(e)
-    #             sys.exit(1)
+    global smtp, serverdatacheck, email, serverhost
+
     if serverdatacheck:
         smtp = smtplib.SMTP(serverhost)
     else:
         print "ERROR: Unable to set up the notification system. Email information is incomplete"
+        shutdown_server()
         sys.exit(1)
     print "Finished smtp setup"
 
@@ -183,11 +152,10 @@ def sendMessage(info, message):
 
         if info in algs.keys():
             for each in algs[info]:
-                Body = "From: %s" % email + "To: %s" % each + "Subject: %s" % message
+                Body = "From: %s" % email
                 try:
                     print "Sending to", info, each
-                    smtp.sendmail(email, each, Body)
-                    print "Email sent"
+                    smtp.sendmail(email, each, 'Subject: %s\n' % info + '\n' + message)
                 except Exception as e:
                     print "Problem sending email", e
         else:
@@ -208,7 +176,7 @@ def message_receiver(message):
     return sendMessage(mes[0], mes[1])
 
 def shutdown_server():
-    """Shuts down flask"""
+    """Shuts down flask server"""
     return 'Server shutting down...'
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
@@ -232,6 +200,7 @@ if __name__ == '__main__':
     try:
         app.run(port=8000)
     except Exception as e:
-        print e
+        print 'ERROR:', e
+        #sys.exit(-1)
 
     print 'Notification Script finished'
